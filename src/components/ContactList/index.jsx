21261aab4b2a7a3e20c23ede8693bb0f12755d6a
svana00 <54863023+svana00@users.noreply.github.com>
2020-12-01 03:30:37 +0000

    non merge conflicts

--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,35 +1,114 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  View, FlatList, Text,
+  View, FlatList, Text, ActivityIndicator, TextInput
 } from 'react-native';
+import filter from 'lodash.filter';
 import styles from './styles';
 import ContactListItem from '../ContactListItem';
 
-const ContactList = ({ contacts }) => (
-  <View style={styles.listContainer}>
-    <Text style={styles.text}>Favorite Contacts</Text>
-    <FlatList
-      numColumns={1}
-      data={contacts}
-      renderItem={({
-        item: {
-          id, name, phoneNumber, thumbnailPhoto,
-        },
-      }) => (
-        <View>
-          <ContactListItem
-            id={id}
-            name={name}
-            phoneNumber={phoneNumber}
-            thumbnailPhoto={thumbnailPhoto}
-          />
-        </View>
-      )}
-      keyExtractor={(contact) => contact.id.toString()}
-    />
-  </View>
-);
+class ContactList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      fullData: props.contacts,
+    };
+  }
+
+  handleSearch(text) {
+    const {
+      fullData,
+    } = this.state;
+    const {
+      updateData,
+    } = this.props;
+    const formattedQuery = text.toLowerCase();
+    const filteredData = filter(fullData, (contact) => this.contains(
+      contact.name.toLowerCase(), formattedQuery,
+    ));
+    updateData(filteredData);
+    this.setQuery(text);
+  }
+
+  setQuery(queryText) {
+    this.setState({ query: queryText });
+  }
+
+  updateFullData(filteredData) {
+    this.setState({ fullData: filteredData });
+  }
+
+  contains(name, query) {
+    console.log(name);
+
+    if (name.includes(query)) {
+      console.log("here");
+      return true;
+    }
+
+    console.log("here2");
+    return false;
+  }
+
+  renderHeader() {
+    const {
+      query,
+    } = this.state;
+
+    return (
+      <View
+        style={{
+          backgroundColor: '#fff',
+          padding: 10,
+          marginVertical: 10,
+          borderRadius: 20,
+        }}
+      >
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          clearButtonMode="always"
+          value={query}
+          onChangeText={(queryText) => this.handleSearch(queryText)}
+          placeholder="Search"
+          style={{ backgroundColor: '#fff', paddingHorizontal: 20 }}
+        />
+      </View>
+    );
+  }
+
+  render() {
+    const {
+      contacts,
+    } = this.props;
+    return (
+      <View style={styles.listContainer}>
+        <Text style={styles.text}>Favorite Contacts</Text>
+        <FlatList
+          ListHeaderComponent={this.renderHeader()}
+          numColumns={1}
+          data={contacts}
+          renderItem={({
+            item: {
+              id, name, phoneNumber, thumbnailPhoto,
+            },
+          }) => (
+            <View>
+              <ContactListItem
+                id={id}
+                name={name}
+                phoneNumber={phoneNumber}
+                thumbnailPhoto={thumbnailPhoto}
+              />
+            </View>
+          )}
+          keyExtractor={(contact) => contact.id.toString()}
+        />
+      </View>
+    );
+  }
+}
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.shape({
